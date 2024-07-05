4bce7bf730442aad61cf9d54526ee464045a0a93
Vince Kennedy <kennedy.v@gmail.com>
2024-07-05 18:38:53 -0400

    Some cleanup.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,11 @@
 class Course < ApplicationRecord
     has_many :holes
+    validates :name, presence: true
+    validates :rating, presence: true
+    validates :slope, presence: true
+    validates :par, presence: true
+
+    validates :holes, :length => { :minimum => 9}
 
     def show
         @course = Course.find(params[:id])
