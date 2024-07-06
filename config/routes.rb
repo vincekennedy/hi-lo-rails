84bdf82699f7bb651bec15a249b8428d662af7e9
Vince Kennedy <kennedy.v@gmail.com>
2024-07-06 10:28:41 -0400

    Finished adding maple creek

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount API::Base, at: "/"
   resources :courses
-  # resources :holes
+  resources :holes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
