781fc19da0a39b01d54736f75218169136ca03af
Vince Kennedy <kennedy.v@gmail.com>
2025-04-02 19:13:46 -0400

    Redirect root URL to show courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount API::Base, at: "/"
+  root to: redirect('/courses/')
   resources :courses
   resources :holes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
